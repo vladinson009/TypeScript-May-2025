42056a152a0d7bdc2dd9ce7bc2c4cbeb897f615d
Vladimir Gulev <vladinson009@icloud.com>
2025-05-20 21:43:19 +0200

    make invalid calls as comments

--- 02.advanced-data-types/05.hotelVisit.ts
+++ 02.advanced-data-types/05.hotelVisit.ts
@@ -65,20 +65,20 @@ visitFloor({ dine() {}, number: 2, hallway: 'C' });
 // Examples above should work
 
 //Examples below should not work
-visitFloor({ train() {}, number: 4, hallway: 'A' });
-visitFloor({ train() {}, number: 1, hallway: 'C', pass: 'Guest' });
-visitFloor({ train() {}, number: 2, hallway: 'A' });
-visitFloor({ train() {}, number: 3, hallway: 'C' });
-visitFloor({ train() {}, number: 3, hallway: 'C', pass: 'Guest' });
-visitFloor({ dine() {}, number: 1, hallway: 'A' });
-visitFloor({ dine() {}, number: 1, hallway: 'B' });
-visitFloor({ dine() {}, number: 1, hallway: 'C' });
-visitFloor({ dine() {}, number: 3, hallway: 'C' });
-visitFloor({ dine() {}, number: 2, hallway: 'C', pass: 'Guest' });
-visitFloor({ dine() {}, number: 1, hallway: 'A', pass: 'Guest' });
-visitFloor({ sleep() {}, number: 3, hallway: 'D' });
-visitFloor({ sleep() {}, number: 4, hallway: 'C' });
-visitFloor({ sleep() {}, number: 1, hallway: 'C' });
-visitFloor({ sleep() {}, number: 1, hallway: 'A' });
-visitFloor({ sleep() {}, number: 2, hallway: 'A' });
-visitFloor({ sleep() {}, number: 2, hallway: 'C' });
+// visitFloor({ train() {}, number: 4, hallway: 'A' });
+// visitFloor({ train() {}, number: 1, hallway: 'C', pass: 'Guest' });
+// visitFloor({ train() {}, number: 2, hallway: 'A' });
+// visitFloor({ train() {}, number: 3, hallway: 'C' });
+// visitFloor({ train() {}, number: 3, hallway: 'C', pass: 'Guest' });
+// visitFloor({ dine() {}, number: 1, hallway: 'A' });
+// visitFloor({ dine() {}, number: 1, hallway: 'B' });
+// visitFloor({ dine() {}, number: 1, hallway: 'C' });
+// visitFloor({ dine() {}, number: 3, hallway: 'C' });
+// visitFloor({ dine() {}, number: 2, hallway: 'C', pass: 'Guest' });
+// visitFloor({ dine() {}, number: 1, hallway: 'A', pass: 'Guest' });
+// visitFloor({ sleep() {}, number: 3, hallway: 'D' });
+// visitFloor({ sleep() {}, number: 4, hallway: 'C' });
+// visitFloor({ sleep() {}, number: 1, hallway: 'C' });
+// visitFloor({ sleep() {}, number: 1, hallway: 'A' });
+// visitFloor({ sleep() {}, number: 2, hallway: 'A' });
+// visitFloor({ sleep() {}, number: 2, hallway: 'C' });
