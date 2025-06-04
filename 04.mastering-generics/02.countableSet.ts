90e6dd9798a6fa29b1dba40b7443a90595a622f8
Vladimir Gulev <vladinson009@icloud.com>
2025-06-04 21:48:50 +0200

    homework

---
+++ 04.mastering-generics/02.countableSet.ts
@@ -0,0 +1,36 @@
+interface CountableSet<T> {
+  add(item: T): void;
+  remove(item: T): void;
+  contains(item: T): boolean;
+  getNumberOfCopies(item: T): number;
+}
+class CountedSet<T> implements CountableSet<T> {
+  private items: Map<T, number> = new Map();
+
+  private getCount<AS extends T>(item: AS): number | undefined {
+    return this.items.get(item);
+  }
+  add(item: T): void {
+    const currentCount = this.getCount(item);
+    currentCount ? this.items.set(item, currentCount + 1) : this.items.set(item, 1);
+  }
+  remove(item: T): void {
+    const currentCount = this.getCount(item);
+    currentCount && this.items.set(item, currentCount - 1);
+  }
+  contains(item: T): boolean {
+    return !!this.getCount(item);
+  }
+  getNumberOfCopies(item: T): number {
+    return this.getCount(item) ?? 0;
+  }
+}
+
+let codesCounterSet = new CountedSet<200 | 301 | 404 | 500>();
+codesCounterSet.add(404);
+codesCounterSet.add(200);
+console.log(codesCounterSet.contains(404));
+console.log(codesCounterSet.getNumberOfCopies(200));
+
+// codesCounterSet.add(205); //TS Error
+// codesCounterSet.getNumberOfCopies(350); //TS Error
